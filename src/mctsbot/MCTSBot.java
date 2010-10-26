8f8defa023ec869e9518f946ed46d58436fbc018
David Moody <david@davidxmoody.com>
2010-10-26 19:19:29 +0100

    Created project, added SimpleBot, added empty class for MCTSBot.

---
+++ src/mctsbot/MCTSBot.java
@@ -0,0 +1,77 @@
+package mctsbot;
+
+import com.biotools.meerkat.Action;
+import com.biotools.meerkat.Card;
+import com.biotools.meerkat.GameInfo;
+import com.biotools.meerkat.Player;
+import com.biotools.meerkat.util.Preferences;
+
+public class MCTSBot implements Player {
+
+	@Override
+	public void actionEvent(int arg0, Action arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dealHoleCardsEvent() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gameOverEvent() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gameStartEvent(GameInfo arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gameStateChanged() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showdownEvent(int arg0, Card arg1, Card arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stageEvent(int arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void winEvent(int arg0, double arg1, String arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Action getAction() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void holeCards(Card arg0, Card arg1, int arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(Preferences arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
